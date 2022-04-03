db1dc90254ae772ec7da8a760d6b8e385896a8cb
akila-niasa <akilaniasa@gmail.com>
2022-04-03 23:01:28 +0600

    homepage added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,7 +3,22 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            
+            <div className='d-flex justify-content-around mt-5'>
+                <div className='mx-2 p-5'>
+                    <h1 className='text-primary'>Your Choice</h1>
+                        <h1 className='mx-5'>Your Laptop
+                    </h1>
+                    <p className='mx-5 container'>Choosing A Laptop Computer That's Right For You Shouldn't Be Difficult. Whether You're A Gamer, A Designer, A Student, Or Simply Need A Laptop For Everyday Tasks And Browsing,You come our website.</p>
+                </div>
+                <div>
+                    <img  src="https://www.startech.com.bd/image/cache/catalog/laptop/chuwi/herobook/herobook-pro/herobook-pro-1-228x228.jpg" alt="" />
+                </div>
+            </div>
+            <div>
+                <h2>Customer Reviews</h2>
+
+                
+            </div>
         </div>
     );
 };
