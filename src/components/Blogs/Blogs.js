98039703b08fb955a85be5f6190f0d2f4f449006
akila-niasa <akilaniasa@gmail.com>
2022-04-04 03:25:58 +0600

    not found added

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -3,10 +3,14 @@ import React from 'react';
 const Blogs = () => {
     return (
         <div>
-            <div>
-                
+            <div className='p-3 m-3'>
+            <h1>What is Context API</h1>
+                <p>The Context API is a way to manage state globally. It used to share data with multiple components, without passing props manually.we need to do is create a context using the createContext function from React.Then we use Provider component to wrap the components that are going to have access to our context.After that,The useContext hook receives a single argument, which is the context that you want to use.</p>
             </div>
-            <div></div>
+            <div className='p-3 m-3'>
+                <h1>What is Semantic Tag</h1>
+                <p>Semantic HTML tags provide information about the contents of those tags how they look on a page.There are some semantic elements which used to define a web page:   <span className='fw-bolder'>Header ,Nav,Section,Article,Aside,Footer</span> in Html5. Header contains logo, heading element.Nav contains navigation link.Section is used to stored introduction,contact information.Article used to element contains.Aside is used as a sidebar to main content.Footer contains upper related document.</p>
+            </div>
         </div>
     );
 };
--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -3,7 +3,7 @@ import React from 'react';
 const NotFound = () => {
     return (
         <div>
-            
+            <img className='h-50 w-50 mt-5 mx-5' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8iSJyJrkabx846XCZlzWzurOrdz6pbIwLFw&usqp=CAU" alt="" />
         </div>
     );
 };
