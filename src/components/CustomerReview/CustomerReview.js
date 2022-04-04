b647b9e6d72f26554d351e0cbb14ba5e1a5515e1
akila-niasa <akilaniasa@gmail.com>
2022-04-04 15:52:07 +0600

    reviews update

--- src/components/CustomerReview/CustomerReview.js
+++ src/components/CustomerReview/CustomerReview.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
 
+
 const CustomerReview = ({review}) => {
     return (
         <div className='col-lg-4 mt-2'>
             <Card style={{ width: '18rem' }}>
-  
+            <Card.Img className='roundedCircle mx-0  h-25 w-25'  src={review.image}/>
   <Card.Body>
     <Card.Title>name:{review.name}</Card.Title>
     <Card.Text>
--- src/components/ReviewDetail/ReviewDetail.js
+++ src/components/ReviewDetail/ReviewDetail.js
@@ -5,7 +5,7 @@ const ReviewDetail = ({review}) => {
     return (
         <div className='col-lg-4 mt-5'>
         <Card style={{ width: '18rem' }}>
-
+        <Card.Img className='roundedCircle mx-0 h-25 w-25' variant="top" src={review.image}/>
 <Card.Body>
 <Card.Title>name:{review.name}</Card.Title>
 <Card.Text>
